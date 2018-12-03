e3893c1ebc8dad2fadd338c78844bc0558cad575
duynguyenkos <nkduyhcmup@gmail.com>
2018-12-03 15:59:41 +0700

    init

---
+++ InterProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterProject.Startup))]
+namespace InterProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
