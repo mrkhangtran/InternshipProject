d391ab3fb752cd8541e5559ce64bf1cec8208d5d
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-03 18:23:30 +0700

    add PODetailModel, ProgressCheckModel update POModel

--- InterProject/DAL/Model/OrderTrack/POModel.cs
+++ InterProject/DAL/Model/OrderTrack/POModel.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace InterProject.DAL.Model.OrderTrack
 {
     public class POModel
     {
+        [Key]
         [Required]
         [Display(Name = "PO Number")]
         [Range(0, 10 ^ 11 - 1)] //up to 10 digits
         //[IDValidation] //Validate Uniqueness
         public float ID { get; set; }
 
+        //unknown
         public string Order { get; set; }
 
         private DateTime defaultOrderDate = DateTime.Now;
 
+        //DropList from 2010 to 2020
         //Default value is Current Date *unfinished*
         [DataType(DataType.DateTime)]
+        [Display(Name = "Order Date")]
         [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime OrderDate
         {
@@ -45,7 +50,7 @@ namespace InterProject.DAL.Model.OrderTrack
 
         [Required]
         [Display(Name = "Port of Delivery")]
-        //[PoDeliveryValidation] // *unfinished*
+        //[PoDeliveryValidation] //PoD must be different from PoL *unfinished*
         public string PortOfDelivery { get; set; }
 
         [StringLength(30, ErrorMessage = "No more than 30 characters")]
@@ -53,21 +58,58 @@ namespace InterProject.DAL.Model.OrderTrack
 
         [StringLength(30, ErrorMessage = "No more than 30 characters")]
         public string Department { get; set; }
+
+        [Display(Name = "Order Type")]
+        [StringLength(30, ErrorMessage = "No more than 30 characters")]
         public string OrderType { get; set; }
 
         //Droplist from 2010 to 2020
         public string Season { get; set; }
+
+        [StringLength(30, ErrorMessage = "No more than 30 characters")]
         public string Factory { get; set; }
 
         //Default value = USD
         public string Currency { get; set; } = "USD";
+
+        [Display(Name = "Ship Date")]
         public DateTime ShipDate { get; set; }
+
+        [Display(Name = "Latest Ship Date")]
+        //Similar or later than ShipDate *unfinished*
         public DateTime LatestShipDate { get; set; }
+
+        [Display(Name = "Delivery Date")]
+        //Similar or later than ShipDate *unfinished*
         public DateTime DeliveryDate { get; set; }
+
+        //unused
         public string LoadType { get; set; }
+
+        //DropList Road-Sea-Air
         public string Mode { get; set; }
 
         [StringLength(30, ErrorMessage = "No more than 30 characters")]
         public string Vendor { get; set; }
+
+        //sum of all PODetails Quantity *not sure if correct*
+        private List<PODetailModel> PODetails { get; set; }
+        [Display(Name = "PO Quantity")]
+        public float POQuantity
+        {
+            get
+            {
+                float sum = 0;
+                foreach (var POItem in PODetails)
+                {
+                    sum += POItem.Quantity;
+                }
+
+                return sum;
+            }
+        }
+
+        //Default value = "New"
+        public string Status { get; set; } = "New";
     }
 }
---
+++ InterProject/DAL/Model/OrderTrack/ProgressCheckModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterProject.DAL.Model.OrderTrack
+{
+    public class ProgressCheckModel
+    {
+        public float ID { get; set; }
+        public string Complete { get; set; }
+        public string onSchedule { get; set; }
+        public DateTime IntendedShipDate { get; set; }
+        public int EstQtyToShip { get; set; }
+        public DateTime InspectionDate { get; set; }
+        public string COMMENTS { get; set; }
+    }
+}
